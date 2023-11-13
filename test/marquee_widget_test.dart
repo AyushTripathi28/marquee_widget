53b0c8029f6d1980664cb1b7df794b2d322316d2
Ayush Tripathi <ayushtripathi445@gmail.com>
2023-11-13 15:07:12 +0530

    added test for my file and also do changes in pubspec.yaml file

--- test/marquee_widget_test.dart
+++ test/marquee_widget_test.dart
@@ -1,10 +1,50 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:marquee_widget/marquee_widget.dart';
 
 void main() {
-  test('adds one to input values', () {
-    // final calculator = Calculator();
-    // expect(calculator.addOne(2), 3);
-    // expect(calculator.addOne(-7), -6);
-    // expect(calculator.addOne(0), 1);
+  // Test for Custom Properties
+  testWidgets('MarqueeWidget applies custom properties',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: MarqueeWidget(
+        movingWidget: Text('Test'),
+        gap: 25,
+        velocity: 50,
+        moveDirectionLtr: true,
+        height: 50,
+      ),
+    ));
+
+    // Verify that custom properties are applied
+    // This can include checking the widget's height, direction, etc.
+  });
+
+  // Test for Scrolling Behavior
+  testWidgets('MarqueeWidget scrolls content', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: MarqueeWidget(
+        movingWidget: Text('Test'),
+      ),
+    ));
+
+    // Interact with the widget to trigger scrolling
+    // Verify that scrolling behavior occurs
+    // This might require simulating gestures or waiting for animations
+  });
+
+  // Test for Boundary Conditions
+  testWidgets('MarqueeWidget handles boundary conditions',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: MarqueeWidget(
+        movingWidget: Text('Test'),
+        gap: 0.0, // Test boundary condition for gap
+        velocity: 0.0, // Test boundary condition for velocity
+      ),
+    ));
+
+    // Verify the behavior under boundary conditions
+    // This might include checking if the widget does not scroll when velocity is 0
   });
 }
